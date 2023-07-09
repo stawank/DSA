15e16e7c44faaffdf291b1d6a1350810dc20cd09
Stawan Chandrashekhar Kulkarni <kulkarns@rptu.de>
2023-07-09 20:45:24 +0200

    swap alternate working

--- BinarySearch/SwapAlternate.cpp
+++ BinarySearch/SwapAlternate.cpp
@@ -1,9 +1,34 @@
 #include<iostream>
+#include<cmath>
+#include<algorithm>
+
 using namespace std;
 
+void swapAlternate (int arr[], int size){
+    int start = 0;
+    for (int i = 0; i < size-1; i = i+2 ) {
+         
+        int temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+
+    }
+
+}
+
+void printArr( int arr[], int size) {
+    cout << " [";
+    for(int i = 0; i < size; i += 1){
+        cout<< " "<< arr[i] ;
+    }
+    cout << "]";
+}
 
 int main(){
-    int inputArr [5] = 
+    int ipArr[6] = {1, 2, 3, 4, 5, 6};
+    printArr(ipArr, 6);
+    swapAlternate(ipArr, 6);
+    printArr(ipArr, 6);
 
 
 
