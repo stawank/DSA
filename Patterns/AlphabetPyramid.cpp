b5414b2cba53f1ef3e29ffd961598a4fa407e2c3
Stawan Chandrashekhar Kulkarni <kulkarns@rptu.de>
2023-09-03 09:18:20 +0200

    New Examples

---
+++ Patterns/AlphabetPyramid.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>> n;
+
+
+    for(int row = 0; row< n; row = row +1){
+        
+        cout<< endl;
+    }
+
+
+
+
+
+    return 0;
+}
